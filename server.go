5f23a02baaea8f9543b48805e9fa658e2b12f99c
mistralll <me.pooh441@gmail.com>
2022-06-17 02:07:12 +0900

    簡単なwikiができた

--- server.go
+++ server.go
@@ -1,29 +1,59 @@
 package main
 
 import (
-  "net/http"
-  "text/template"
+	"io/ioutil"
+	"net/http"
+	"text/template"
 )
 
 type Page struct {
-  Title string
-  Count int
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+  filename := title + ".txt"
+  body, err := ioutil.ReadFile(filename)
+  if err != nil {
+    return nil, err
+  }
+  return &Page{Title:title, Body:body}, nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-  page := Page{"Hello World.", 1}
-  tmpl, err := template.ParseFiles("layout.html") // ParseFilesを使う
-  if err != nil {
-    panic(err)
-  }
+	title := r.URL.Path[6:]
+  p, _ := loadPage(title)
 
-  err = tmpl.Execute(w, page)
+  t, _ := template.ParseFiles("view.html")
+  t.Execute(w, p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[6:]
+  p, err := loadPage(title)
   if err != nil {
-    panic(err)
+    p = &Page{Title: title}
   }
+  t, _ := template.ParseFiles("edit.html")
+  t.Execute(w, p)
+}
+
+func saveHanler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[6:]
+  body := r.FormValue("body")
+  p := &Page{Title:title, Body:[]byte(body)}
+  p.save()
+  http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
 func main() {
-  http.HandleFunc("/", viewHandler) // hello
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHanler)
+	http.ListenAndServe(":8080", nil)
 }
